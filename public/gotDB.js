dd6ba914d8e56bef636b9d3e8d018873b1d8ae09
William Crews <crewswh@gmail.com>
2021-09-06 15:30:20 -0400

    going for the win

--- public/gotDB.js
+++ public/gotDB.js
@@ -47,25 +47,28 @@ var request = indexedDB.open("industrialGrind");
 function connectAB() {
     console.log("hello B!");
 
-request.onupgradeneeded = function() {
-    // The database did not previously exist, so create object stores and indexes.
-    const db = request.result;
-    const store = db.createObjectStore("books", {keyPath: "isbn"});
-    const titleIndex = store.createIndex("by_title", "title", {unique: true});
-    const authorIndex = store.createIndex("by_author", "author");
+    request.onupgradeneeded = function(event) {
+        // The database did not previously exist, so create object stores and indexes.
+        const db = event.target.result;
+        db.createObjectStore('getMonz', {autoIncrement: true})
+        
+        // const store = db.createObjectStore("books", {keyPath: "isbn"});
+        // const titleIndex = store.createIndex("by_title", "title", {unique: true});
+        // const authorIndex = store.createIndex("by_author", "author");
+      
+        // // Populate with initial data.
+        // store.put({title: "Quarry Memories", author: "Fred", isbn: 123456});
+        // store.put({title: "Water Buffaloes", author: "Fred", isbn: 234567});
+        // store.put({title: "Bedrock Nights", author: "Barney", isbn: 345678});
+      
+        console.log("here")
+      };
+
   
-    // Populate with initial data.
-    store.put({title: "Quarry Memories", author: "Fred", isbn: 123456});
-    store.put({title: "Water Buffaloes", author: "Fred", isbn: 234567});
-    store.put({title: "Bedrock Nights", author: "Barney", isbn: 345678});
-  
-    console.log("here")
-  };
-  
-  request.onsuccess = function() {
-    db = request.result;
-    console.log("dank")
-  };
+    request.onsuccess = function(event) {
+        db = event.target.result;
+        console.log("dank")
+    };
 }
 
 window.addEventListener("onload", connectAB())
