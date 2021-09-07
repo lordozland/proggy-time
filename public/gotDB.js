a69573c318c821fe631953fd4c93459afa8d595d
William Crews <crewswh@gmail.com>
2021-09-06 23:30:08 -0400

    working on it

--- public/gotDB.js
+++ public/gotDB.js
@@ -1,14 +1,3 @@
-// let db;
-
-// https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
-// https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/getAll
-// https://www.w3.org/TR/IndexedDB/
-// https://www.youtube.com/watch?v=n4E4kjQIw6M
-// https://stackoverflow.com/questions/19609173/indexeddb-reference-error-db-is-not-defined
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-
-// var windowVar = (/*...*/);
-
 if (indexedDB) {
     console.log("IndexedDB is supported");
 }
@@ -17,8 +6,9 @@ else {
 };
 
 
+
+var request = indexedDB.open("industrialGrind");\
 let db;
-var request = indexedDB.open("industrialGrind");
 
 request.onupgradeneeded = function(event) {
     var db = event.target.result;
@@ -74,19 +64,13 @@ function playerSave() {
 }
 
 
-
-
-// transaction.oncomplete = function(event) {
-//     console.log("All done!");
-//   };
-
 window.addEventListener('online', playerSave)
 
-// var objectStore = transaction.objectStore("customers");
-// customerData.forEach(function(customer) {
-//   var request = objectStore.add(customer);
-//   request.onsuccess = function(event) {
-//     // event.target.result === customer.ssn;
-//   };
-// });
 
+
+// https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+// https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/getAll
+// https://www.w3.org/TR/IndexedDB/
+// https://www.youtube.com/watch?v=n4E4kjQIw6M
+// https://stackoverflow.com/questions/19609173/indexeddb-reference-error-db-is-not-defined
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
